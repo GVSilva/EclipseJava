e6a08ea3d1c4cd4f7ef595fdcb5a9df30b56d272
Gabriel Vasco <87551481+GVSilva@users.noreply.github.com>
2021-10-04 23:46:48 -0300

    Add files via upload

--- atvs/Alunos.java
+++ atvs/Alunos.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Alunos {
 	public static void main(String[] Args) {
 
-		final int Total_Alunos = 4;
-		final int Total_bim = 4;
-		final double Nota_min = 70.0;
+		final int total_Alunos = 4;
+		final int total_bim = 4;
+		final double nota_min = 70.0;
 
 		Scanner entrada = new Scanner(System.in);
-		String[] nomeAlunos = new String[Total_Alunos];
-		double[][] notaAlunos = new double[Total_Alunos][Total_bim];
-		double[] mediaAlunos = new double[Total_Alunos];
+		String[] nomeAlunos = new String[total_Alunos];
+		double[][] notaAlunos = new double[total_Alunos][total_bim];
+		double[] mediaAlunos = new double[total_Alunos];
 
 		// Obter nome dos Alunos.
 
-		for (int i = 0; i < Total_Alunos; i++) {
+		for (int i = 0; i < total_Alunos; i++) {
 			System.out.println("Informe o nome do " + (i + 1) + "° aluno:");
 			nomeAlunos[i] = entrada.nextLine();
 
@@ -25,8 +25,8 @@ public class Alunos {
 
 		// Obter nota dos alunos em todos os bimestres.
 
-		for (int i = 0; i < Total_Alunos; i++) {
-			for (int j = 0; j < Total_bim; j++) {
+		for (int i = 0; i < total_Alunos; i++) {
+			for (int j = 0; j < total_bim; j++) {
 				System.out.println("Informe a nota do " + nomeAlunos[i] + " para o " + (j + 1) + "°  bimestre");
 				notaAlunos[i][j] = entrada.nextDouble();
 
@@ -34,19 +34,19 @@ public class Alunos {
 		}
 
 		// Calcular a média dos alunos.
-		for (int i = 0; i < Total_Alunos; i++) {
-			for (int j = 0; j < Total_bim; j++) {
+		for (int i = 0; i < total_Alunos; i++) {
+			for (int j = 0; j < total_bim; j++) {
 				mediaAlunos[i] = +notaAlunos[i][j];
 			}
-			mediaAlunos[i] /= Total_bim;
+			mediaAlunos[i] /= total_bim;
 		}
 
 		// Mostrar a situação dos alunos.
 
 		System.out.println("========= RESULTADO FINAL =========");
 
-		for (int i = 0; i < Total_Alunos; i++) {
-			if (mediaAlunos[i] >= Nota_min) {
+		for (int i = 0; i < total_Alunos; i++) {
+			if (mediaAlunos[i] >= nota_min) {
 				System.out.println("Nome: " + nomeAlunos[i] + " Media: " + mediaAlunos[i] + " Situação: Aprovado");
 				continue;
 			}
--- atvs/Dialogo.java
+++ atvs/Dialogo.java
@@ -10,13 +10,13 @@ public class Dialogo {
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.print("Qual é o seu nome? ");
-		String Firstname = entrada.next();		
+		String firstname = entrada.next();		
 		System.out.print("Qual seu ultimo nome? ");
-		String Lastname = entrada.next();
+		String lastname = entrada.next();
 		System.out.print("Qual é a sua idade? ");
 		int Idade = entrada.nextInt();
 		
-		System.out.println("Olá " + Firstname + " " + Lastname + " de " + Idade + " de idade.");
+		System.out.println("Olá " + firstname + " " + lastname + " de " + Idade + " de idade.");
 	}
 
 }
